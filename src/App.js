8870b93ad77148820982e7e9c3bfc044d4ebe17f
Michelle Yang <myang292@gmail.com>
2022-03-16 12:55:19 -0700

    Group 2 - Michelle Yang

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './styles/App.css'
 import StopLight from './components/StopLight'
 import Highway from './components/Highway'
