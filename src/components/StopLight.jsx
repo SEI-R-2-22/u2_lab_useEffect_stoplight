3dbb3784f2f2944d80f41ec6db986de89fcc272c
Yusong Shi <jackshi.sys@gmail.com>
2022-03-16 13:02:48 -0400

    Group 2 Yusong

--- src/components/StopLight.jsx
+++ src/components/StopLight.jsx
@@ -1,20 +1,32 @@
-const StopLight = (props) => {
+const StopLight = ({lights, setLights}) => {
 
+  
   return (
     <div className="action-area">
       <h1>Stoplight useEffect</h1>
       <div className="stoplight-container">
         <div className="stoplight">
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className={lights.red ? 'red' : undefined}></div>
+          <div className={lights.yellow ? 'yellow' : undefined}></div>
+          <div className={lights.green ? 'green' : undefined}></div>
         </div>
         <div>
           <button
             className="countdown"
             // onCLick here
+            onClick={()=> {
+              if(lights.red === true && lights.yellow === true && lights.green === true) {
+                setLights({red: false, yellow: false, green: false})
+              } else if (lights.red === true && lights.yellow === true) {
+                setLights({...lights, green: true})
+              } else if (lights.red === true) {
+                setLights({...lights, yellow: true})
+              } else {
+                setLights({...lights, red: true})
+              }
+            }}
           >
-          Countdown
+          {lights.red && lights.yellow && lights.green ? "Reset" : "Countdown"}
           </button>
         </div>
       </div>
